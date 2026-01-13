f728a50d7ce845c73fa032ec29fbac4f0fc00d1c
Komol Parpixodjayev <www.komol8689@gmail.com>
2026-01-13 16:22:50 +0500

    aadd

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,6 @@
 export const config = {
-    BACKEND_URL: String(import.meta.env.VITE_BACKEND_URL || 'http://localhost:3030/api/v1'),
-    FRONTEND_URL: String(import.meta.env.VITE_FRONTEND_URL || 'http://localhost:5050')
+    BACKEND_URL: String(import.meta.env.VITE_BACKEND_URL || 'https://komol.uz/api/v1'),
+    FRONTEND_URL: String(import.meta.env.VITE_FRONTEND_URL || 'https://komol.uz'),
 };
+
+console.log(config);
